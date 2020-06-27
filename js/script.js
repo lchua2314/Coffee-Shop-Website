019b2b4146fed4bfd9b9e455497add1e08a73854
lchua2314 <lchua2314@gmail.com>
2020-06-26 17:18:11 -0700

    Refined code

--- js/script.js
+++ js/script.js
@@ -7,24 +7,14 @@ const passwordIn = document.querySelector("#pswrd");
 const loginForm = document.querySelector("#login-form");
 
 function createNewUsername() {
-  let username =
-      'Currently logged in as: <br> <i class="fas fa-user"></i> ' +
-      usernameIn.value,
-    password = passwordIn.value;
+  let password = passwordIn.value;
   if (usernameIn.value === "") {
     usernameOut.innerHTML = "Please enter a username.";
   } else if (password.length < 8) {
     usernameOut.innerHTML =
       "Please enter a password that is at least 8 character long.";
   } else {
-    usernameOut.innerHTML = username;
-    usernameOutNavBar.innerHTML =
-      '<i class="fas fa-user"></i> ' + usernameIn.value;
     Storage.setUsername(usernameIn.value);
-    usernameIn.value = "";
-    passwordIn.value = "";
-    sendBtn.innerHTML = "Sign Out";
-    loginForm.innerHTML = "";
     location.reload();
   }
 }
@@ -38,32 +28,30 @@ class Storage {
   }
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-  let user = Storage.getUsername();
-  if (user) {
-    usernameOutNavBar.innerHTML = '<i class="fas fa-user"></i> ' + user;
-    if (loginForm) {
-      loginForm.innerHTML = "";
-      usernameOut.innerHTML =
-        'Currently logged in as: <br> <i class="fas fa-user"></i> ' + user;
-    }
+let user = Storage.getUsername();
+if (user) {
+  usernameOutNavBar.innerHTML = '<i class="fas fa-user"></i> ' + user;
+  if (loginForm) {
+    loginForm.innerHTML = "";
+    usernameOut.innerHTML =
+      'Currently logged in as: <br> <i class="fas fa-user"></i> ' + user;
   }
+}
 
-  if (sendBtn) {
-    if (!user) {
-      sendBtn.addEventListener("click", createNewUsername);
-      loginForm.addEventListener("submit", (event) => {
-        event.preventDefault();
-      });
-    } else {
-      sendBtn.innerHTML = "Sign Out";
-      sendBtn.addEventListener("click", () => {
-        localStorage.clear();
-        location.reload();
-      });
-      loginForm.addEventListener("submit", (event) => {
-        event.preventDefault();
-      });
-    }
+if (sendBtn) {
+  if (!user) {
+    sendBtn.addEventListener("click", createNewUsername);
+    loginForm.addEventListener("submit", (event) => {
+      event.preventDefault();
+    });
+  } else {
+    sendBtn.innerHTML = "Sign Out";
+    sendBtn.addEventListener("click", () => {
+      localStorage.removeItem("username");
+      location.reload();
+    });
+    loginForm.addEventListener("submit", (event) => {
+      event.preventDefault();
+    });
   }
-});
+}
